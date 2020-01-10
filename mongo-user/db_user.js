0e56756f283daf4922e64523574bf71d7ae6ba9c
Nijraj Gelani <gelaninijraj@gmail.com>
2020-01-10 13:45:13 +0530

    NCTE-713 Added default users and scopes

--- mongo-user/db_user.js
+++ mongo-user/db_user.js
@@ -12,25 +12,30 @@ if (db.getUser('cteadmin') == null) {
 }
 
 db.schedules.insert({
-    "_cls": "PeriodicTask",
-    "name": "INTERNAL INDICATOR AGING TASK",
-    "enabled": true,
-    "args": [],
-    "task": "cte.tasks.indicator_aging_task.age_indicators",
-    "interval": {
-        "every": 12,
-        "period": "hours",
+    _cls: "PeriodicTask",
+    name: "INTERNAL INDICATOR AGING TASK",
+    enabled: true,
+    args: [],
+    task: "cte.tasks.indicator_aging_task.age_indicators",
+    interval: {
+        every: 12,
+        period: "hours",
     },
+})
+
+db.settings.insert({
+    proxy: null,
+    logLevel: "info"
 })
 
 db.users.insert({
     username: "admin",
     password: "$2y$12$hwULodJIcg6ncfRgjWkqnOcJFEcSEk3zMiIyjxQLgRZwXbROVilF.",
-    scopes: ["read", "write"]
+    scopes: ["read", "write", "me"]
 });
 
 db.users.insert({
     username: "user",
     password: "$2y$12$hwULodJIcg6ncfRgjWkqnOcJFEcSEk3zMiIyjxQLgRZwXbROVilF.",
-    scopes: ["read"]
+    scopes: ["read", "me"]
 });
