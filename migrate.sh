abb3128503c54be979b0807b323b0df7ccdd82e7
Nijraj Gelani <nijraj.gelani@crestdatasys.com>
2021-01-27 12:52:42 +0530

    WIP added command line argument

--- migrate.sh
+++ migrate.sh
@@ -4,7 +4,12 @@ set -e
 MOUNT_PATH=`grep "/data/db:z" docker-compose.yml`
 MONGO_DIR=`docker exec -ti core python -c 'import sys; print(sys.argv[1].strip().split(":")[0][1:].strip(), end="")' "${MOUNT_PATH}"`
 
-if [ ! -d "$MONGO_DIR" ]; then
+if [ -z ${1+x} ]; then
+    echo "Path to previous docker-compose dir passed"
+    PREVIOUS_MOUNT_PATH=`grep "/data/db:z" $1/docker-compose.yml`
+    PREVIOUS_MONGO_DIR=`docker exec -ti core python -c 'import sys; print(sys.argv[1].strip().split(":")[0][1:].strip(), end="")' "${MOUNT_PATH}"`
+    echo $PREVIOUS_MONGO_PATH
+elif [ ! -d "$MONGO_DIR" ]; then
     echo "Stopping running containers"
     docker stop core ui mongodb
     echo "Extracting databse from existing container"
